6d864c3349864d786f3e2622faaf93a981c0a324
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-11-11 14:34:08 +0100

    get monitoring data from one machine within a day

--- Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Controller/MonitoringDataController.java
+++ Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Controller/MonitoringDataController.java
@@ -4,11 +4,9 @@ import Q3Project.DataAPIApplication.Interface.IMonitoringDataService;
 import Q3Project.DataAPIApplication.Interface.ITreeviewService;
 import Q3Project.DataAPIApplication.Model.MonitoringData202009;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -16,12 +14,17 @@ import java.util.List;
 @RequestMapping("/api")
 public class MonitoringDataController {
 
-//    @Autowired
-//    private IMonitoringDataService service;
-//
-//    @GetMapping("/monitoringdata")
-//    public List<MonitoringData202009> GetAllMonitoringData(){
-//        return service.GetAllFromMachines();
-//    }
+    @Autowired
+    private IMonitoringDataService service;
+
+    @GetMapping("/monitoringdatas/{board}/{port}")
+    public List<MonitoringData202009> GetAllMonitoringData(@PathVariable(value = "board") int board, @PathVariable(value = "port") int port){
+        return service.GetAllFromMachine(board, port);
+    }
+
+    @GetMapping("/monitoringdata/{board}/{port}/{datetime}")
+    public List<MonitoringData202009> GetAllMonitoringDataPerDay(@PathVariable(value = "board") int board, @PathVariable(value = "port") int port, @PathVariable(value = "datetime") String datetime) throws ParseException {
+        return service.GetAllFromMachinePerDay(board, port, datetime);
+    }
 
 }
--- Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Services/MachineMonitoringService.java
+++ Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Services/MachineMonitoringService.java
@@ -6,39 +6,41 @@ import Q3Project.DataAPIApplication.Model.ProductionData;
 import Q3Project.DataAPIApplication.Repository.MonitoringData202009Repository;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
-public class MachineMonitoringService /*implements IMonitoringDataService*/ {
+public class MachineMonitoringService implements IMonitoringDataService {
 
     private final MonitoringData202009Repository monitoringData202009Repository;
-    private final ProductionDataService productionDataService;
 
-    public MachineMonitoringService(MonitoringData202009Repository monitoringData202009Repository, ProductionDataService productionDataService) {
+    public MachineMonitoringService(MonitoringData202009Repository monitoringData202009Repository) {
         this.monitoringData202009Repository = monitoringData202009Repository;
-        this.productionDataService = productionDataService;
     }
 
-    public List<MonitoringData202009> GetAll() {
-        return monitoringData202009Repository.findAll();
+    @Override
+    public List<MonitoringData202009> GetAllFromMachine(int board, int port) {
+        return monitoringData202009Repository.findByBoardPort(board, port);
     }
 
-    //@Override
-    //public List<MonitoringData202009> GetAllByBoardPort(long treeviewId) {
-        //List<ProductionData> productionDatas = productionDataService.GetAllByBoardPort(board, port);
-    //}
-
-    //@Override
-    public List<MonitoringData202009> GetAllFromMachines() {
-        List<MonitoringData202009> allTreeviews = GetAll();
-        //allTreeviews.removeIf(item -> item.getTreeviewSoortId() != TreeviewTypes.MACHINE.treeviewSoortId);
-        return allTreeviews;
+    @Override
+    public List<MonitoringData202009> GetAllFromMachinePerDay(int board, int port, String datetime) throws ParseException {
+        List<MonitoringData202009> dataFromOneMachine = monitoringData202009Repository.findByBoardPort(board, port);
+        Date requestedDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(datetime);
+        dataFromOneMachine.removeIf(item -> item.getTimestamp().before(requestedDate));
+        Calendar c = Calendar.getInstance();
+        c.setTime(requestedDate);
+        c.add(Calendar.DATE, 1);
+        Date finalRequestedDate = c.getTime();
+        dataFromOneMachine.removeIf(item -> item.getTimestamp().after(finalRequestedDate));
+        return dataFromOneMachine;
     }
 
-    //@Override
-    public List<MonitoringData202009> GetAllFromComponents() {
-        List<MonitoringData202009> allTreeviews = GetAll();
-        //allTreeviews.removeIf(item -> item.getTreeviewSoortId() != TreeviewTypes.COMPONENT.treeviewSoortId);
-        return allTreeviews;
+    @Override
+    public List<MonitoringData202009> GetAllFromComponents(List<ProductionData> productionData) {
+        return null;
     }
 }
