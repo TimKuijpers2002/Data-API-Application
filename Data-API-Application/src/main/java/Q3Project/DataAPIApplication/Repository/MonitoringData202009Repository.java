eb3a2165d2489721a9a4d01a3a248800eda9588d
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-11-18 11:38:34 +0100

    changed query for monitoringdata

--- Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Repository/MonitoringData202009Repository.java
+++ Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Repository/MonitoringData202009Repository.java
@@ -4,12 +4,13 @@ import Q3Project.DataAPIApplication.Model.MonitoringData202009;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
 @Repository
 public interface MonitoringData202009Repository extends JpaRepository<MonitoringData202009, Long> {
-    @Query(value="SELECT m.timestamp, m.shotTime FROM MonitoringData202009 m WHERE m.board= ?1 AND m.port = ?2")
+    @Query(value="SELECT m FROM MonitoringData202009 m WHERE m.board= ?1 AND m.port = ?2")
     List<MonitoringData202009> findByBoardPort(int board, int port);
 }
