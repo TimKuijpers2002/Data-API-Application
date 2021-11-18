e5e09a08489fd93892c93f84a525c2eb37a274b9
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-11-18 10:47:30 +0100

    changed query for monitoringdata

--- Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Repository/MonitoringData202009Repository.java
+++ Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Repository/MonitoringData202009Repository.java
@@ -4,7 +4,6 @@ import Q3Project.DataAPIApplication.Model.MonitoringData202009;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
