6d08758f97d8430261f5330291b43ce872eb6649
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-11-25 16:13:35 +0100

    added Param

--- Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Repository/MachineMonitoringPoortenRepository.java
+++ Data-API-Application/src/main/java/Q3Project/DataAPIApplication/Repository/MachineMonitoringPoortenRepository.java
@@ -1,9 +1,9 @@
 package Q3Project.DataAPIApplication.Repository;
 
 import Q3Project.DataAPIApplication.Model.MachineMonitoringPoorten;
-import Q3Project.DataAPIApplication.Model.MonitoringData202009;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface MachineMonitoringPoortenRepository extends JpaRepository<MachineMonitoringPoorten, Long> {
 
-    @Query(value="SELECT m FROM MachineMonitoringPoorten m WHERE m.name= ?1")
-    MachineMonitoringPoorten findByName(String name);
+    @Query(value="SELECT m FROM MachineMonitoringPoorten m WHERE m.name= :name")
+    MachineMonitoringPoorten findByName(@Param("name") String name);
 }
